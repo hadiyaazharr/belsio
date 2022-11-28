c9607ce66ac176319801ae00925fff57ec32b90f
Morning <hadiya.azhar@prosignstech.com>
2022-11-28 18:51:26 +0500

    'angular-pages'

---
+++ src/app/pages/auth/login/login.page.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '@lib/services';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.css'],
+})
+export class LoginPage {
+
+  public _background: string = '../../assets/images//background.png';
+  private _callbackURL: string;
+
+  constructor(
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private _authService: AuthService,
+  ) {
+    this._callbackURL = this._activatedRoute.snapshot.queryParamMap.get('callbackURL') || `/`;
+  }
+
+  onClickSignIn(): void {
+    this._authService.login();
+    this._router.navigate([this._callbackURL]);
+  }
+}
